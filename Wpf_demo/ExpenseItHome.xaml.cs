7f3307b2786ac680e63b8de15394c15434a6d1e0
tom <254449149@qq.com>
2017-12-01 20:26:50 +0800

    wpf hello world

---
+++ Wpf_demo/ExpenseItHome.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf_demo
+{
+    /// <summary>
+    /// ExpenseItHome.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExpenseItHome : Page
+    {
+        public ExpenseItHome()
+        {
+            InitializeComponent();
+        }
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // 查看报告页
+            ExpenseReportPage expenseReportPage =new ExpenseReportPage(this.peopleListBox.SelectedItem);
+            this.NavigationService.Navigate(expenseReportPage);
+        }
+    }
+}
---
+++ Wpf_demo/ExpenseReportPage.xaml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace Wpf_demo
+{
+    /// <summary>
+    /// ExpenseReportPage.xaml 的交互逻辑
+    /// </summary>
+    public partial class ExpenseReportPage : Page
+    {
+        public ExpenseReportPage()
+        {
+            InitializeComponent();
+        }
+        // Custom constructor to pass expense report data
+        public ExpenseReportPage(object data) : this()
+        {
+            // Bind to expense report data.
+            this.DataContext = data;
+        }
+        private void DataGrid_Loaded(object sender, RoutedEventArgs e)
+        {
+            // View Expense Report
+            DataGrid dataGrid = FindName("DataGrid1") as DataGrid;
+            Double length = dataGrid.ActualWidth/dataGrid.Columns.Count;
+            foreach (var column in dataGrid.Columns)
+            {
+                column.Width = length-1;
+            }
+        }
+    }
+}
